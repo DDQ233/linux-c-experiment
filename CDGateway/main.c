ae2871650ba391d2ad9b7ac966176ffc6a123712
DDQ233 <2841730354@qq.com>
2020-06-21 16:35:54 +0800

    添加了串口接收数据转发到MQTT服务器功能

--- CDGateway/main.c
+++ CDGateway/main.c
@@ -9,6 +9,7 @@
 #include <time.h>
 #include <MQTTAsync.h>
 #include <mysql/mysql.h>
+#include <cjson/cJSON.h>
 #include "mysqlutil.h"
 #include "mqttutil.h"
 #include "serialportutil.h"
@@ -51,6 +52,8 @@
 static unsigned int isMysqlConnect = 0;
 static unsigned int isMqttConnect = 0;
 static unsigned int isAllSerialportConnect = 0;
+static unsigned int deviceStatus[DEVICE_NUM];
+static char sensorMqttTopic[DEVICE_NUM][20];
 static int uartFdPool[DEVICE_NUM];
 static pthread_t serialportThreadPool[DEVICE_NUM];
 static MYSQL mysqlClient;
@@ -107,6 +110,8 @@ void onSend(void* context, MQTTAsync_successData* response)
 
 int main()
 {
+    // Function declaration
+    void initDeviceStatus();
     void *serialportThread(void *arg);
     void initSerialportList();
     void startSerialportThread();
@@ -117,41 +122,83 @@ int main()
     void startMqttService();
     void stopMqttService();
 
+    // Function initialization
+    initDeviceStatus();
     // openSerialportList();
     initSerialportList();
     startMySqlService();
     startMqttService();
-
     startSerialportThread();
 
     sleep(TWO_SECOND);
 
-    closeSerialportThread();
-    closeSerialport();
-    stopMysqlService();
-    stopMqttService();
+    int i = 0;
+    int ret;
+    for (i = 0; i < DEVICE_NUM; i++) {
+        ret = pthread_join(serialportThreadPool[i], NULL);
+        if (ret < 0) {
+            printf("> x Failed to join number of %d pthread.\n", i);
+        } else {
+            printf("> O Number of %d thread was joined.\n", i);
+        }
+    }
+
+    // closeSerialportThread();
+    // closeSerialport();
+    // stopMysqlService();
+    // stopMqttService();
 
     return 0;
+}
+
+void initDeviceStatus()
+{
+    printf("> O Initializing all device status.\n");
+    int i = 0;
+    for (i = 0; i < DEVICE_NUM; i++) {
+        deviceStatus[i] = 0;
+    }
+    printf("> O Finished.\n\n");
 }
 
 
 void *serialportThread(void *arg)
 {
     char receiveBuffer[128];
-    // int uartNum = *((int*)arg);
-    int uartNum = *((int*)arg);
+    int deviceNum = *((int*)arg);
     int count = 0;
+    int ret;
+    cJSON *json, *json_value;
+    MQTTAsync_message message = MQTTAsync_message_initializer;
+    MQTTAsync_responseOptions opts = MQTTAsync_responseOptions_initializer;
+    opts.onSuccess = onSend;
+	opts.onFailure = onSendFailure;
+	opts.context = mqttClientList[deviceNum];
+    printf("> O Thread %d running.\n", deviceNum);
     while(1) {
-        count = read(uartFdPool[uartNum], receiveBuffer, 128);
+        count = read(uartFdPool[deviceNum], receiveBuffer, 128);
         if (count > 0) {
-            printf("UART : %d > %s", uartNum, receiveBuffer);
+            printf("UART %d > %s", deviceNum, receiveBuffer);
             // Send message to mqtt server
-            
-            /*
-             * Do something 
-             */
-
-            count = 0;
+            message.payload = receiveBuffer;
+            message.payloadlen = (int)strlen(receiveBuffer);
+            message.qos = MQTT_MESSAGE_QOS;
+            // Device auth ? Device Status ?
+            json = cJSON_Parse(receiveBuffer);
+            json_value = cJSON_GetObjectItem(json, "uid");
+            if (sensorMqttTopic[deviceNum] == NULL) {
+                sprintf(sensorMqttTopic[deviceNum], MQTT_SENSOR_TOPIC_PERFIX);
+                strcat(sensorMqttTopic[deviceNum], json_value->valuestring);
+                printf("> Topic -----> %s", sensorMqttTopic[deviceNum]);
+            }
+            // Can topic make it static and save in memory ? 
+            if ((ret = MQTTAsync_sendMessage(mqttClientList[deviceNum], sensorMqttTopic[deviceNum], &message, &opts)) != MQTTASYNC_SUCCESS) {
+                printf("> x Failed to send message. Error code : %d\n", ret);
+            } else {
+                printf("> Send -----> %s.\n", receiveBuffer);
+            }
+            memset(receiveBuffer, 0, sizeof(receiveBuffer));
+            count = 0; 
         }
     }
 }
@@ -208,6 +255,7 @@ void startSerialportThread()
         } else {
             printf("> O Number of %d thread was created.\n", i);
         }
+        sleep(HALF_SECOND);
     }
     printf("> O Finished.\n\n");
 }
@@ -217,19 +265,11 @@ void closeSerialportThread()
     printf("> O Closing serial port thread.\n");
     int i = 0;
     int ret;
-    for (i = 0; i < DEVICE_NUM; i++) {
-        // ret = pthread_join(serialportThreadPool[i], NULL);
-        // if (ret < 0) {
-        //     printf("> x Failed to join number of %d pthread.\n", i);
-        // } else {
-        //     printf("> O Number of %d thread was joined.\n", i);
-        // }
-        ret = pthread_cancel(serialportThreadPool[i]);
-        if (ret < 0) {
-            printf("> x Failed to cancel number of %d pthread.\n", i);
-        } else {
-            printf("> O Number of %d thread was canceled.\n", i);
-        }
+    ret = pthread_cancel(serialportThreadPool[i]);
+    if (ret < 0) {
+        printf("> x Failed to cancel number of %d pthread.\n", i);
+    } else {
+        printf("> O Number of %d thread was canceled.\n", i);
     }
     printf("> O Finished.\n\n");
 }
@@ -294,7 +334,11 @@ void stopMqttService()
 {
     printf("> O Stoping mqtt service.\n");
     int i =0;
+    MQTTAsync_disconnectOptions disc_opts;
+    disc_opts = bindDisconnectOptions(onDisconnect, onDisconnectFailure);
     for (i = 0; i < DEVICE_NUM; i++) {
+        MQTTAsync_disconnect(mqttClientList[i], &disc_opts);
+        printf("> O Number of %d mqtt client disconnect finished.\n", i);
         MQTTAsync_destroy(&mqttClientList[i]);
         printf("> O Number of %d mqtt client destroy finished.\n", i);
     }
